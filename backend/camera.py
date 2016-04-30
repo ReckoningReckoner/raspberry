632c09af1f0023d9d18a955a05e582d120cacfab
viraj <viraj@3f@gmail.com>
2016-04-30 02:08:06 -0400

    Taking a photo is now on a separate thread. Saves 200 last photos

--- backend/camera.py
+++ backend/camera.py
@@ -1,11 +1,11 @@
 import subprocess
 import time
 import os
-from os import listdir
-import sys
+from threading import Thread
+
 
 directory = "static/photos"
-max_album_size = 20
+max_album_size = 200
 
 if __debug__:
     if not os.path.isdir(directory):
@@ -26,23 +26,29 @@ def sync_photo():
 
 def get_sorted_photos():
     photos = []
-    for f in sorted(listdir(directory)):
+    for f in sorted(os.listdir(directory)):
         if f.endswith(".jpg") or f.endswith(".jpeg"):
             photos.append(f)
 
     return photos
 
 
-def take_photo():
+def photograph_and_sync():
     filename = str(int(time.time()))
     files = get_sorted_photos()
     if len(files) > max_album_size:
         for f in files[0:len(files)-max_album_size]:
             subprocess.call(["rm", "-f", directory + "/" + f])
 
-    subprocess.call(["fswebcam", "-r 720x480",
+    subprocess.call(["fswebcam", "-r 640x480",
                      directory + "/" + filename + ".jpg"])
     sync_photo()
+
+
+def take_photo():
+    # Threading this
+    photo_t = Thread(target=photograph_and_sync)
+    photo_t.start()
 
 
 def get_newest_photo():
